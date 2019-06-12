572dd55f6cf399833a422777f7553338d37c2a25
Baptiste Dumont <baptiste.dumont@epitech.eu>
2019-06-12 13:49:23 +0200

    fix with route and not url

--- django/drivingschool/drivingschool/login.py
+++ django/drivingschool/drivingschool/login.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 @login_required
 def home(request):
     for group in request.user.groups.all():
         if group.name == "Secretary":
-            return HttpResponseRedirect('/')
+            return redirect('home')
         if group.name == "Admin":
-            return HttpResponseRedirect('/admin')
+            return redirect('/admin')
 
 
