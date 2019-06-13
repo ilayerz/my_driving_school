2067b5233ebebd5660f5bd316615a47eb0c4c87a
Michael Johnson <andry-michael.johnson@epitech.eu>
2019-06-13 12:26:46 +0100

    init

--- django/drivingschool/drivingapp/models.py
+++ django/drivingschool/drivingapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Planning(models.Model)
+	id_student.IntegerField(max_length=100)
+	id_instructor.IntegerField(max_length=100)
+	date.CharField(max_length=100)
+	lieu.CharField(max_length=100)
---
+++ django/drivingschool/drivingschool/student.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+def getPlanning:
+
+
+def sendForm:
+	
