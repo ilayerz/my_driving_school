acdfc6775c5c2ea3252bfa665e6bdaf0bb357816
Baptiste Dumont <baptiste.dumont@epitech.eu>
2019-06-14 10:50:51 +0200

    add datepicker

--- django/drivingschool/drivingapp/forms.py
+++ django/drivingschool/drivingapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PlanningForm(forms.Form):
-	date = forms.CharField(label="date", max_length=100)
+	date = forms.CharField(label="date",max_length=100, widget=forms.DateInput(attrs={'class': 'datepicker'}))
 	lieu = forms.CharField(label="lieu", max_length=100)
